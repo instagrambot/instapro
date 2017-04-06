f88111b870ab11cc7232aa705b91cfc236dcb289
Okhlopkov Daniil Olegovich <ohl.d@yandex.ru>
2017-04-06 22:38:53 +0300

    add Parser to package init

--- instabot/__init__.py
+++ instabot/__init__.py
@@ -1,7 +1,9 @@
 from .user import User
 from .api import API
 from .bot import Bot
+from .parser import Parser
 
 assert User  # silence pyflakes
 assert API  # silence pyflakes
 assert Bot  # silence pyflakes
+assert Parser
