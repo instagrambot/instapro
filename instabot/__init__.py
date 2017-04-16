b13e9993742d00bebe4baef4cb0edeb11ce51c21
ohld <ohl.d@yandex.ru>
2017-04-16 22:09:32 +0300

    add Bot class template

--- instabot/__init__.py
+++ instabot/__init__.py
@@ -2,3 +2,10 @@ from .user import User, UserController
 from .api import api
 from .getter import Getter
 from .sender import Sender
+from .bot import Bot
+
+assert User
+assert UserController
+assert Getter
+assert Sender
+assert Bot
