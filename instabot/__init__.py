99e8622db4660cbf0a67f19c652f1706c7aaa111
Alexander Mitropolsky <mitrop98@gmail.com>
2017-04-16 21:30:35 +0300

    parser -> getter

--- instabot/__init__.py
+++ instabot/__init__.py
@@ -1,10 +1,10 @@
 from .user import User, UserController
 from .api import api
-from .parser import Parser
+from .parser import Getter
 # from .bot import Bot
 
 assert User  # silence pyflakes
 assert UserController  # silence pyflakes
 assert api  # silence pyflakes
-assert Parser
+assert Getter
 # assert Bot  # silence pyflakes
