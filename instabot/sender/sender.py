8311b68dc2ee9858cf46ce7c50af48b1f972ea4e
ohld <ohl.d@yandex.ru>
2017-04-24 12:30:40 +0300

    populate with delay param and tqdm.
    also improve stability

--- instabot/sender/sender.py
+++ instabot/sender/sender.py
@@ -1,6 +1,8 @@
 import time
 import json
 import warnings
+from random import random
+from tqdm import tqdm
 
 from random import random
 
@@ -24,19 +26,22 @@ class Sender(object):
             self.controller.main = self.controller.load_user(username)
         self.main = self.controller.main  # for shortage
 
+    def sleep(self, delay):
+        time.sleep(delay * 0.5 + random() * delay)
+
     def can_follow(self, usr=None):
         # check filter
         # check limit
         # check counters
         if usr is not None and isinstance(usr, dict):
-            if not hasattr(self.main, "following"):
+            if not hasattr(self.main, 'following'):
                 self.main.following = set(
-                    [usr["pk"] for usr in self.get.user_following(self.main.id)])
-            if usr["pk"] in self.main.following:
+                    [usr['pk'] for usr in self.get.user_following(self.main.id)])
+            if usr['pk'] in self.main.following:
                 return False
-            if "is_business" in usr and usr["is_business"]:
+            if 'is_business' in usr and usr['is_business']:
                 return False
-            if "is_verified" in usr and usr["is_verified"]:
+            if 'is_verified' in usr and usr['is_verified']:
                 return False
 
         return True
@@ -46,47 +51,61 @@ class Sender(object):
         # check limit
         # check counters
         if md is not None:
-            if "has_liked" in md and md["has_liked"]:
+            if 'has_liked' in md and md['has_liked']:
                 return False
         return True
 
-    def follow_followers(self, main_target, total=None):
+    def follow_followers(self, main_target, total=None, delay=15):
         if not str(main_target).isdigit():
             main_target = self.get.user_info(main_target)['pk']
-        return self.follow_users(self.get.user_followers(main_target, total=total))
+        return self.follow_users(self.get.user_followers(main_target, total=total), delay=delay)
 
-    def follow_following(self, main_target, total=None):
+    def follow_following(self, main_target, total=None, delay=15):
         if not str(main_target).isdigit():
             main_target = self.get.user_info(main_target)['pk']
-        return self.follow_users(self.get.user_following(main_target, total=total))
+        return self.follow_users(self.get.user_following(main_target, total=total), delay=delay)
 
-    def follow_users(self, targets):
-        for target in targets:
-            if self.follow(target) is None:
-                warnings.warn("Error while following %s." % target["username"])
-            time.sleep(10)  # sleep for user.delay
+    def follow_users(self, targets, delay=15):
+        for target in tqdm(targets, desc='follow users'):
+            res = self.follow(target)
+            if res is None:
+                warnings.warn('Error while following %s.' % target['username'])
+            if res:
+                self.sleep(delay)
+        self.main.save()
         return False  # exitcode 0 - no errors
 
     def follow(self, target):
         if self.can_follow(target):
-            print("follow %s" % target['username'])
-            return Request.send(self.controller.main.session, 'friendships/create/' + str(target['pk']) + '/', '{}')
+            ret = Request.send(self.controller.main.session, 'friendships/create/' + str(target['pk']) + '/', '{}')
+            if 'follows' not in self.main.counters:
+                self.main.counters.follows = 0
+            self.main.counters.follows += 1
+            return ret
         return False
 
     def like(self, media):
         if self.can_like(media):
-            print("like %s's media" % media['user']['username'])
-            return Request.send(self.controller.main.session, 'media/' + str(media['pk']) + '/like/', '{}')
+            ret = Request.send(self.controller.main.session,
+                               'media/' + str(media['pk']) + '/like/', '{}')
+            if 'likes' not in self.main.counters:
+                self.main.counters.likes = 0
+            self.main.counters.likes += 1
+            return ret
         return False
 
-    def like_medias(self, medias):
-        for media in medias:
-            if self.like(media) is None:
-                warnings.warn("Error while liking %s's media." %
-                              media['user']['pk'])
+    def like_medias(self, medias, delay=5):
+        for media in tqdm(medias, 'like medias'):
+            res = self.like(media)
+            if res is None:
+                warnings.warn(
+                    "Error while liking %s's media." % media['user']['username'])
+            if res:
+                self.sleep(delay)
+        self.main.save()
         return False  # exitcode 0 - no errors
 
-    def like_geo_medias(self, location, total=None):
+    def like_geo_medias(self, location, total=None, delay=5):
         if not str(location).isdigit():
             location = self.get.geo_id(location)
-        return self.like_medias(self.get.geo_medias(location, total=total))
+        return self.like_medias(self.get.geo_medias(location, total=total), delay=delay)
